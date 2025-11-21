7096f887bf6714c201fba76942fa511c5fdf97e3
Musta0712 <adrimustatea@gmail.com>
2025-11-21 09:26:05 +0100

    .

---
+++ Mustatea_Adrian_Tarea7/script.js
@@ -0,0 +1,28 @@
+// 1.- Crear un objeto persona con nombre, edad y ciudad. Acceder a sus propiedades con . y con [ ]. Cambiar el valor de edad y agregar una nueva propiedad profesión.
+
+class Person {
+    constructor (nombre, edad, ciudad) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.ciudad = ciudad;
+    }
+
+    person = new Person("Diego", 27, "Madrid");
+
+};
+
+// 2. Acceder a sus propiedades con . 
+console.log(persona.nombre);  // "Diego"
+console.log(persona.edad);    // 27
+
+// 3. Acceder a sus propiedades con [ ]
+console.log(persona["ciudad"]); // "Madrid"
+
+// 4. Cambiar el valor de edad
+persona.edad = 31;
+
+// 5. Agregar una nueva propiedad profesión
+persona.profesion = "Ingeniero";
+
+// Mostrar el objeto actualizado
+console.log(persona);
