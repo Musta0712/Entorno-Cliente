2e37cb3722dbcb7786f6c730d7e34c9125e213b3
Musta0712 <adrimustatea@gmail.com>
2026-02-23 10:21:44 +0100

    .

--- mi-primer-react/src/App.js
+++ mi-primer-react/src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const titulo = <h1>Mi título</h1>
+  const calculo = <h2>{sum(4,6)}</h2>;
+  return <div className='title'>{titulo}{calculo}</div>
+
+  function sum(x, y) {
+    return x + y;
+  }
 }
 
 export default App;
