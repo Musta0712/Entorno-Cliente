19e19c278e72e676ae006fae68f1228d56d7cb2c
Musta0712 <adrimustatea@gmail.com>
2026-02-27 09:33:06 +0100

    .

--- mi-primer-react/src/App.js
+++ mi-primer-react/src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import './App.css';
 
 function App() {
+  /*
   const [contador, setContador] = useState(0);
 
   const titulo = <h1>Mi título</h1>;
@@ -25,6 +26,27 @@ function App() {
       {calculo}
       <button onClick={sum}>Botón</button>
       <button onClick={() => setContador(contador + 1)}>Contador: {contador}</button>
+    </div>
+  );
+  */
+
+  // 1. Crea un contador con dos botones (incrementar y decrementar). Muestra el número en un h1
+  const [contador, setContador] = useState(0);
+
+  const incrementar = () => {
+    setContador(contador + 1);
+  };
+
+  const decrementar = () => {
+    setContador(contador - 1);
+  };
+
+  return (
+    <div>
+      <h1>{contador}</h1>
+
+      <button onClick={incrementar}>Incrementar</button>
+      <button onClick={decrementar}>Decrementar</button>
     </div>
   );
 }
