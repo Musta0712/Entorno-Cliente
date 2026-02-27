3a625e909ea1a55d8b839588113311889e687404
Musta0712 <adrimustatea@gmail.com>
2026-02-27 09:16:30 +0100

    .

--- mi-primer-react/src/App.js
+++ mi-primer-react/src/App.js
@@ -1,13 +1,32 @@
+import { useState } from "react";
 import './App.css';
 
 function App() {
-  const titulo = <h1>Mi título</h1>
-  const calculo = <h2>{sum(4,6)}</h2>;
-  return <div className='title'>{titulo}{calculo}</div>
+  const [contador, setContador] = useState(0);
 
-  function sum(x, y) {
+  const titulo = <h1>Mi título</h1>;
+
+  function sum1(x, y) {
     return x + y;
   }
+
+  const calculo = <h2>{sum1(4, 6)}</h2>;
+
+  const sum = () => {
+    const a = 5;
+    const b = 9;
+    const resultado = a + b;
+    alert(resultado);
+  };
+
+  return (
+    <div className="title">
+      {titulo}
+      {calculo}
+      <button onClick={sum}>Botón</button>
+      <button onClick={() => setContador(contador + 1)}>Contador: {contador}</button>
+    </div>
+  );
 }
 
 export default App;
